69df19c4c60363bbc79c71355f5f5799d38d5653
Syed Shaaz Abdullah <152294259+abdullahusernamenotavailable@users.noreply.github.com>
2026-01-21 18:31:42 +0000

    learning datatypes

---
+++ 01_javascript/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+//don't use alert, we are using nodejs not browser
+
+/*console.log(3+
+    3 --> code readibility should be high
+);
+*/
+let name = "Abdullah"
+let age = "20"
+let isLoggedIn = false
+//null --> standalone value -> empty defined
+//undefined --> value not defined
+//symbol -->unique
+
+//object
+
+console.log(typeof "Abdullah");
