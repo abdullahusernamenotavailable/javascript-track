c4af0249f34ece538a10a0446a745cc2aead7e81
Syed Shaaz Abdullah <152294259+abdullahusernamenotavailable@users.noreply.github.com>
2026-01-22 14:15:15 +0000

    day 2 learnings

---
+++ 02_javascript/02Learnings.js
@@ -0,0 +1,51 @@
+//operations 
+
+// let value = 3
+// let negValue = -value
+
+//console.log(2+2) or 2-2 or 2*2 (basic mathematic operations just as any other programming language)
+
+// let str1 = "Abdullah"
+// let str2 = "Student"
+
+// console.log(str1 + str2)
+// console.log("2" + 1 + 1)
+
+//Learned how to write a clean code, readibility matters!
+
+//Learned comparisons >,<, == , >= , != , already know this from python
+//------------------------------------
+
+//Stack (Primitive), Heap (non primitive)
+
+// let myName = "Syed"
+// let another = myName
+// another = "Abdullah"
+
+// let user = {
+//     email : "user@google.com",
+//     upi : "user1@sbi"
+
+// }
+
+// let user2 = user
+
+// user2.email = "google@google.com"
+// console.log(user.email);
+// console.log(user2.email)
+
+//----------------------------------------------
+//STRINGS
+
+// const name = "Abdullah"
+// const repoCount = 50
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+//Learned different Types of methods of strings like length, casing(upper or lower), charAt and indexof and etc, going pretty smoothe 
+
+// const name = "      abd         "
+// console.log(name.trim()); //removes empty space opening and closing
+
+//.replace and .includes
+
+
