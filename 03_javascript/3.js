83e25a3ea0abdaca62d75a95eefbd7fbf429f486
Syed Shaaz Abdullah <152294259+abdullahusernamenotavailable@users.noreply.github.com>
2026-01-25 14:27:38 +0000

    day3

---
+++ 03_javascript/3.js
@@ -0,0 +1,20 @@
+//actually forgot to commit changes that i learned like yesterday
+//funny as this would not look good for a daily track, but here i am and i am going to start arrays today!
+
+// const myArr = [0,1,2,3,4,5, true,  "Abdullah"]
+// console.log(myArr[4]);
+
+// const arr = new Array(1,2,3,4,5)
+
+//Methods
+
+// myArr.push()
+// myArr.pop()
+// myArr.unshift()
+//myArr.shift() --> removes the val
+//includes --> true or false
+//indexof(9) --> -1 if not available in array
+//,join() --> stringify
+//slice -> slices a part of array and prints
+//splice -> changes the original array
+
